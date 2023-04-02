eb2d4f9f8a94a36b448656acfca80b0c6545746f
WGint <asd63458@gmail.com>
2023-04-02 11:56:52 +0800

    localstorage 範例

--- dist/js/todo.js
+++ dist/js/todo.js
@@ -1,23 +1,20 @@
 let addBtn = document.querySelector('#add-btn');
 let itemName = document.querySelector('#item-name');
 let item = document.querySelector('#item');
-
 item.addEventListener('click', (e) => {
     let target = e.target;
-    let tag = targrt.tagname.toLowerCase();
-
-    // 因為結構上input 與 div 上一層都是li
+    let tag = target.tagName.toLowerCase();
+    // 因為結構上 input 與 div 上一層都是 li
     if (tag == 'input' || tag == 'div') {
         target = target.parentNode;
         tag = target.tagName.toLowerCase();
     }
-
-    // 當為li 時表示處理該項目，過濾點到 Ul
+    // 當為 li 時表示處理該項目，過濾點到 UL
     if (tag == 'li') {
         let isDone = target.classList.contains('delete');
         if (isDone) {
             // 變成未完成
-            arget.classList.remove('delete');
+            target.classList.remove('delete');
             target.querySelector('input').checked = false;
         } else {
             // 變成完成
@@ -26,21 +23,16 @@ item.addEventListener('click', (e) => {
         }
     }
 })
-
 const appendItem = (name) => {
     let li = document.createElement('li');
     let input = document.createElement('input');
     let div = document.createElement('div');
-
     div.innerHTML = name;
     input.type = 'checkbox';
-
     li.appendChild(input);
     li.appendChild(div);
-
     item.appendChild(li); // 效能比較好
 }
-
 const appendItemES6 = (name) => {
     let li = `<li>
         <input type="checkbox">
@@ -48,8 +40,7 @@ const appendItemES6 = (name) => {
     </li>`;
     item.innerHTML += li; // 效能比較差
 }
-
-addBtn.addEventListener('click', () => {
+const addItem = () => {
     if (!valid()) {
         reset();
         return;
@@ -57,33 +48,42 @@ addBtn.addEventListener('click', () => {
     let name = itemName.value;
     // appendItem(name);
     appendItemES6(name);
-
     reset();
-
     // 寫法二
     // if (valid()) {
     //     let name = itemName.value;
     //     appendItemES6(name);
     // }
 }
-
 addBtn.addEventListener('click', () => {
     addItem();
 })
-
 itemName.addEventListener('keyup', (e) => {
     let key = e.key.toLowerCase();
-
     if (key == 'enter') {
         addItem();
     }
 })
-
 const valid = () => {
     return itemName && itemName.value;
 }
-
 const reset = () => {
     itemName.value = '';
     itemName.focus();
 }
+
+
+
+let todoData = [];
+todoData.push({
+    status: 'done',
+    value: '測試'
+});
+
+let todoDataToString = JSON.stringify(todoData);
+localStorage.setItem('todo', todoDataToString);
+console.table(todoData);
+
+let todoDataRestore = JSON.parse(localStorage.getItem('todo'));
+console.log('%c 還原', 'color: red; font-size: 30px;');
+console.table(todoDataRestore);

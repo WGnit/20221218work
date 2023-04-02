bdde57484ed3a5722f0d19613df0a7d91ecde1ed
WGint <asd63458@gmail.com>
2023-04-02 15:09:57 +0800

    刪除功能與索引遺失問題修正

--- dist/js/todo_vue.js
+++ dist/js/todo_vue.js
@@ -2,24 +2,32 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            items: [
-                { status: 'pending', value: 1111 },
-                { status: 'done', value: 2222 },
-            ],
+            storageKey: 'todo',
+            items: [],
         }
     },
     methods: {
-
         addItem() {
             if (!this.valid()) {
                 this.reset();
                 return;
             }
             // 新增資料
+            this.items.push({
+                status: 'pending',
+                value: this.getName()
+            })
             this.reset();
+            this.save();
+        },
+        getName() {
+            if (this.$refs.itemName) {
+                return this.$refs.itemName.value;
+            }
+            return '';
         },
         valid() {
-            return this.$refs.itemName && this.$refs.itemName.value;
+            return (this.getName());
         },
         reset() {
             let itemName = this.$refs.itemName;
@@ -29,9 +37,38 @@ createApp({
             }
         },
         changeStatus(index) {
+            let item = this.items[index];
+            if (!item) {
+                return;
+            }
             let status = this.items[index].status == 'pending' ? 'done' : 'pending';
             this.items[index].status = status;
+            this.save();
             console.table(this.items);
+        },
+        removeItem(index) {
+            this.items.splice(index, 1);
+            this.save();
+        },
+        restore() {
+            try {
+                let todo = localStorage.getItem(this.storageKey);
+                if (!todo) {
+                    todo = [];
+                } else {
+                    todo = JSON.parse(todo);
+                }
+                this.items = todo;
+            } catch (e) {
+                this.items = [];
+            }
+        },
+        save() {
+            let data = JSON.stringify(this.items);
+            localStorage.setItem(this.storageKey, data);
         }
+    },
+    mounted() {
+        this.restore();
     }
 }).mount('#app')
